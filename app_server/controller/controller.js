b785fbb0655f09af0d16e9ceb7659ae27438153e
Sezer Furkan Kirpitci <sezer.furkan.kirpitci@gmail.com>
2021-07-30 15:15:55 +0300

    EJS görüntü motoru kullanılarak MVC bir yapı oluşturuldu.

--- app_server/controller/controller.js
+++ app_server/controller/controller.js
@@ -1,10 +1,15 @@
 var path =require ('path');
 
 module.exports.index = function(req,res){
-    res.render('index')
+    //Rendew ile views klasörüne bakılır. 2. parametre ise json dosyasıdır.
+    var name = ['Ahmet','Ali','Ayşe','Ayten'];
+    res.render('index', {
+        message : "Bu bir test mesajıdır.",
+        name : name
+    });
 }
 module.exports.login = function(req,res){
-    res.render('login')
+    res.render('login');
 }
 
 module.exports.signin = function(req,res){
